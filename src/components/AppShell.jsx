2e98c183e031162160c05bead64f7ea18c32525d
Jyoti Gupta <jg8073931@gmail.com>
2026-02-07 21:10:07 +0530

    Fix Sidebar import path

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import Sidebar from '@/components/Sidebar';
+import Sidebar from "./Sidebar";
 import Footer from '@/components/FooterSection';
 import AboutUsModal from '@/components/AboutUsModal';
 import ContactUsModal from '@/components/ContactUsModal';
